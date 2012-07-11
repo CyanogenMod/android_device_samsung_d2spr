849ab08789de11e153f2d385658d6311ba804e8a
Shareef Ali <shareefalis@gmail.com>
2012-07-11 01:35:54 -0500

    d2: add inline kernel building
    Change-Id: I8eeefce37f7ccaee153dcea47467c28a4147cbda

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,8 @@
 TARGET_OTA_ASSERT_DEVICE := d2spr
 
 # Kernel
-TARGET_PREBUILT_KERNEL      := device/samsung/d2spr/prebuilt/kernel
+TARGET_KERNEL_SOURCE    := kernel/samsung/d2
+TARGET_KERNEL_CONFIG    := cyanogen_d2_defconfig
 BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom user_debug=31
 BOARD_KERNEL_BASE           := 0x80200000
 BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
--- device.mk
+++ device.mk
@@ -20,29 +20,6 @@ $(call inherit-product-if-exists, vendor/samsung/d2spr/d2spr-vendor.mk)
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/d2spr/overlay
 
-# Kernel and modules
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-    LOCAL_KERNEL := device/samsung/d2spr/prebuilt/kernel
-else
-    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel \
-    device/samsung/d2spr/modules/ansi_cprng.ko:system/lib/modules/ansi_cprng.ko \
-    device/samsung/d2spr/modules/btlock.ko:system/lib/modules/btlock.ko \
-    device/samsung/d2spr/modules/dhd.ko:system/lib/modules/dhd.ko \
-    device/samsung/d2spr/modules/dma_test.ko:system/lib/modules/dma_test.ko \
-    device/samsung/d2spr/modules/exfat_core.ko:system/lib/modules/exfat_core.ko \
-	device/samsung/d2spr/modules/exfat_fs.ko:system/lib/modules/exfat_fs.ko \
-    device/samsung/d2spr/modules/msm-buspm-dev.ko:system/lib/modules/msm-buspm-dev.ko \
-    device/samsung/d2spr/modules/qce40.ko:system/lib/modules/qce40.ko \
-    device/samsung/d2spr/modules/qcedev.ko:system/lib/modules/qcedev.ko \
-    device/samsung/d2spr/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
-    device/samsung/d2spr/modules/reset_modem.ko:system/lib/modules/reset_modem.ko \
-    device/samsung/d2spr/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    device/samsung/d2spr/modules/spidev.ko:system/lib/modules/spidev.ko
-
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
 
