d2a8468cf8243c2a6230eb857f9d7b58c18fe99a
Dan Pasanen <dan.pasanen@gmail.com>
2013-10-28 21:28:42 -0500

    d2spr: remove cdma.mk
    Change-Id: I25b23acb3474e2af63fb5ae84379b97c0c5bfb60

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
