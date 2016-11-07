9e6628a685b05459eb4ff2c367d0908246408fdb
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-07 11:11:04 -0600

    d2spr: remove dupe full_base_telephony.mk inclusion
    * this is in d2-common
    
    Change-Id: I6d39479b2ef4285eb72abafbc6ff4b59e23db7a3
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- full_d2spr.mk
+++ full_d2spr.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
  
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d2spr device
 $(call inherit-product, device/samsung/d2spr/device.mk)
 
